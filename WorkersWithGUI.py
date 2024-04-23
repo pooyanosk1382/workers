fb3510b7f363718c624280a08d9b904dec0e9af8
Pooyan <117625732+pooyanosk1382@users.noreply.github.com>
2024-04-23 22:19:14 +0330

    let's work on gui

--- WorkersWithGUI.py
+++ WorkersWithGUI.py
@@ -1,11 +1,63 @@
 import tkinter as tk
+import threading
+import time
+
+startTime = time.time()
+store = threading.RLock()
+ship = threading.RLock()
+eatingWorkers = 0
+t = 60
+
+
+class Workers(threading.Thread):
+    def __init__(self, name, speed):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.speed = speed
+        self.number = 0
+        self.eaten = False
+
+    def run(self) -> None:
+        global eatingWorkers
+        currentTime = time.time()
+        while currentTime - startTime < t + t/16:
+            if currentTime - startTime >= t / 2 and self.eaten is False and eatingWorkers < 4:
+                self.eating()
+            self.loading()
+            self.put()
+            currentTime = time.time()
+
+    def eating(self):
+        global eatingWorkers
+        eatingWorkers += 1
+        self.eaten = True
+        line1.set(f"Line 1: Event 1 occurred")
+        print('worker ' + self.name + ' is eating.')
+        print('There are ' + str(eatingWorkers) + ' workers that are eating')
+        time.sleep(t/16)
+        eatingWorkers -= 1
+
+    def put(self):
+        store.acquire()
+        print('worker ' + self.name + ' is putting.')
+        store.release()
+        self.number += 3
+        print('worker ' + self.name + ' is backing to ship.')
+        time.sleep(self.speed)
+
+    def loading(self):
+        ship.acquire()
+        print('worker ' + self.name + ' is loading.')
+        ship.release()
+        print('worker ' + self.name + ' is going to store.')
+        time.sleep(self.speed)
+
 
 # Function to update the lines
 def update_lines():
     # Simulating backend events
     # You can replace this with your actual backend event handling logic
     # For demonstration purposes, I'm just updating the lines with dummy data
-    line1.set(f"Line 1: Event 1 occurred")
     line2.set(f"Line 2: Event 2 occurred")
     line3.set(f"Line 3: Event 3 occurred")
     line4.set(f"Line 4: Event 4 occurred")
@@ -53,4 +105,41 @@ label8.pack(pady=10)  # Add vertical padding
 update_lines()
 
 # Run the main loop
+
+
+worker1 = Workers('1', 5)
+worker2 = Workers('2', 7)
+worker3 = Workers('3', 9)
+worker4 = Workers('4', 6)
+worker5 = Workers('5', 6.5)
+worker6 = Workers('6', 4.75)
+worker7 = Workers('7', 8)
+worker8 = Workers('8', 6)
+
+worker1.start()
+worker2.start()
+worker3.start()
+worker4.start()
+worker5.start()
+worker6.start()
+worker7.start()
+worker8.start()
+
 root.mainloop()
+worker1.join()
+worker2.join()
+worker3.join()
+worker4.join()
+worker5.join()
+worker6.join()
+worker7.join()
+worker8.join()
+
+print(worker1.number)
+print(worker2.number)
+print(worker3.number)
+print(worker4.number)
+print(worker5.number)
+print(worker6.number)
+print(worker7.number)
+print(worker8.number)
